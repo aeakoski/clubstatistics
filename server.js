f53f00a2fc9bbedfa619c23cd1a3e93efa9f0bc9
Koski <koski@LAPTOP-3CKB7AMM.localdomain>
2023-07-04 13:32:46 +0200

    add authentication to access gui

--- server.js
+++ server.js
@@ -271,6 +271,32 @@ app.get('/stats', async (req, res) => {
 });
 
 
+app.use((req, res, next) => {
+
+  // -----------------------------------------------------------------------
+  // Authentication middleware
+
+  const auth = {login: process.env.CLUB_MEMBER_USER || "medlem", password: process.env.CLUB_MEMBER_PASSWORD || "lösen"}
+
+  // Parse login and password from headers
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+  // Verify login and password are set and correct
+  if (login && password && login === auth.login && password === auth.password) {
+    // Access granted...
+    return next()
+  }
+
+  // Access denied...
+  res.set('WWW-Authenticate', 'Basic realm="401"') // change this
+  res.status(401).send('Authentication required.') // custom message
+
+  // -----------------------------------------------------------------------
+
+})
+
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
